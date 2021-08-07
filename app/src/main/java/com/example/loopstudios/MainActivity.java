0a99c38d0c4575ecb3bdb698260bbe6bd96ce4e6
thirza258 <thirzahmad@gmail.com>
2021-08-07 15:35:04 +0700

    -add

--- app/src/main/java/com/example/loopstudios/MainActivity.java
+++ app/src/main/java/com/example/loopstudios/MainActivity.java
@@ -3,12 +3,93 @@ package com.example.loopstudios;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private RelativeLayout relativeLayout, relInteractive, relDeepEarth;
+    private RelativeLayout relNightArcade, relSoccerTeam, relGrid, relAbove;
+    private RelativeLayout relBorealis, relFisheye, relFooter;
+    private LinearLayout linear1, linear2, linear3;
+    private ImageButton icon_hamburger, facebook, twitter, pinterest, instagram;
+    private ImageView image_hero, logo, image_interactive, image_deep_earth;
+    private ImageView image_night_arcade, image_soccer_team, image_grid, image_from_above;
+    private ImageView image_pocket_borealis, image_curiosity, image_fisheye;
+    private ImageView bgBlack, logo2;
+    private Button see_all;
+    private TextView immersive, leader, founded, deep_earth, night_arcade;
+    private TextView soccer_team, grid, borealis, above, curiosity, fisheye;
+    private TextView about, careers, events, products, support, license;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+
+    }
+
+    public void initView() {
+        Log.d(TAG, "initView: Started");
+
+        relativeLayout = findViewById(R.id.relativeLayout);
+        relInteractive = findViewById(R.id.relInteractive);
+        relDeepEarth = findViewById(R.id.relDeepEarth);
+        relNightArcade = findViewById(R.id.relNightArcade);
+        relSoccerTeam = findViewById(R.id.relSoccerTeam);
+        relGrid = findViewById(R.id.relGrid);
+        relAbove = findViewById(R.id.relAbove);
+        relBorealis = findViewById(R.id.relBorealis);
+        relFisheye = findViewById(R.id.relFisheye);
+        relFooter = findViewById(R.id.relFooter);
+        linear1 = findViewById(R.id.linear1);
+        linear2 = findViewById(R.id.linear2);
+        linear3 = findViewById(R.id.linear3);
+        icon_hamburger = findViewById(R.id.icon_hamburger);
+        facebook = findViewById(R.id.facebook);
+        twitter = findViewById(R.id.twitter);
+        pinterest = findViewById(R.id.pinterest);
+        instagram = findViewById(R.id.instagram);
+        image_hero = findViewById(R.id.image_hero);
+        logo = findViewById(R.id.logo);
+        image_interactive = findViewById(R.id.image_interactive);
+        image_deep_earth = findViewById(R.id.image_deep_earth);
+        image_night_arcade = findViewById(R.id.image_night_arcade);
+        image_soccer_team = findViewById(R.id.image_soccer_team);
+        image_grid = findViewById(R.id.image_grid);
+        image_from_above = findViewById(R.id.image_from_above);
+        image_pocket_borealis = findViewById(R.id.image_pocket_borealis);
+        image_curiosity = findViewById(R.id.image_curiosity);
+        image_fisheye = findViewById(R.id.image_fisheye);
+        bgBlack = findViewById(R.id.bgBlack);
+        logo2 = findViewById(R.id.logo2);
+        see_all = findViewById(R.id.see_all);
+        immersive = findViewById(R.id.immersive);
+        leader = findViewById(R.id.leader);
+        founded = findViewById(R.id.founded);
+        deep_earth = findViewById(R.id.deep_earth);
+        night_arcade = findViewById(R.id.night_arcade);
+        soccer_team = findViewById(R.id.soccer_team);
+        grid = findViewById(R.id.grid);
+        borealis = findViewById(R.id.borealis);
+        above = findViewById(R.id.above);
+        curiosity = findViewById(R.id.curiosity);
+        fisheye = findViewById(R.id.fisheye);
+        about = findViewById(R.id.about);
+        careers = findViewById(R.id.careers);
+        events = findViewById(R.id.events);
+        products = findViewById(R.id.products);
+        support = findViewById(R.id.support);
+        license = findViewById(R.id.license);
+
     }
 }
