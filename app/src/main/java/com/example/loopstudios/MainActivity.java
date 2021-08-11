616a2f857e12d66ac8fcb7896094cd11c4b83d2f
thirza258 <thirzahmad@gmail.com>
2021-08-11 15:27:32 +0700

    -add

--- app/src/main/java/com/example/loopstudios/MainActivity.java
+++ app/src/main/java/com/example/loopstudios/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.loopstudios;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.OrientationEventListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -18,17 +21,23 @@ public class MainActivity extends AppCompatActivity {
     private RelativeLayout relativeLayout, relInteractive, relDeepEarth;
     private RelativeLayout relNightArcade, relSoccerTeam, relGrid, relAbove;
     private RelativeLayout relBorealis, relFisheye, relFooter, relCuriosity;
-    private LinearLayout linear1, linear2, linear3, linear4, linear5, linear6, linear7, linear8;
+    private RelativeLayout relaot, reldate, relbeach, relburger, relcoca;
+    private RelativeLayout relgradient, relnoragami, relsao;
+    private LinearLayout linear1, linear2, linear3, linear4, linear5, linear6, linear7, linear8, linear9, linear10, linear11;
     private ImageButton icon_hamburger, facebook, twitter, pinterest, instagram;
     private ImageView image_hero, logo, image_interactive, image_deep_earth;
     private ImageView image_night_arcade, image_soccer_team, image_grid, image_from_above;
     private ImageView image_pocket_borealis, image_curiosity, image_fisheye;
+    private ImageView image_date, image_aot, image_beach, image_burger;
+    private ImageView image_coca, image_gradient, image_noragami, image_sao;
     private ImageView bgBlack, logo2;
     private Button see_all;
     private TextView immersive, leader, founded, deep_earth, night_arcade;
     private TextView soccer_team, grid, borealis, above, curiosity, fisheye;
     private TextView about, careers, events, products, support, license;
     private TextView aboutFooter, careersFooter, eventsFooter, supportFooter, productFooter;
+    private TextView aot, date, beach, burger, coca;
+    private TextView gradient, noragami, sao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,12 +90,131 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        icon_hamburger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        see_all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Started");
 
 
 
+            }
+        });
 
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        support.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+        products.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+        careers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+        events.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        View view = getWindow().getDecorView();
+        int orientation = getResources().getConfiguration().orientation;
+        if (Configuration.ORIENTATION_LANDSCAPE == orientation) {
+            //Do SomeThing; // Landscape
+            see_all.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    see_all.setVisibility(View.GONE);
+                    linear10.setVisibility(View.VISIBLE);
+                    linear11.setVisibility(View.VISIBLE);
+                }
+            });
+        } else {
+            //Do SomeThing;  // Portrait
+            see_all.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    see_all.setVisibility(View.GONE);
+                    relaot.setVisibility(View.VISIBLE);
+                    relbeach.setVisibility(View.VISIBLE);
+                    reldate.setVisibility(View.VISIBLE);
+                    relnoragami.setVisibility(View.VISIBLE);
+                    relsao.setVisibility(View.VISIBLE);
+                    relgradient.setVisibility(View.VISIBLE);
+                    relcoca.setVisibility(View.VISIBLE);
+                    relburger.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+    }
+
+    public void onConfigurationChanged(Configuration configuration) {
+        super.onConfigurationChanged(configuration);
+
+        if(configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            see_all.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    see_all.setVisibility(View.GONE);
+                    linear10.setVisibility(View.VISIBLE);
+                    linear11.setVisibility(View.VISIBLE);
+                }
+            });
+
+        }
+        else {
+            see_all.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    see_all.setVisibility(View.GONE);
+                    relaot.setVisibility(View.VISIBLE);
+                    relbeach.setVisibility(View.VISIBLE);
+                    reldate.setVisibility(View.VISIBLE);
+                    relnoragami.setVisibility(View.VISIBLE);
+                    relsao.setVisibility(View.VISIBLE);
+                    relgradient.setVisibility(View.VISIBLE);
+                    relcoca.setVisibility(View.VISIBLE);
+                    relburger.setVisibility(View.VISIBLE);
+                }
+            });
+        }
 
     }
+
+
 
     public void initView() {
         Log.d(TAG, "initView: Started");
@@ -151,92 +279,39 @@ public class MainActivity extends AppCompatActivity {
         productFooter = findViewById(R.id.productsFooter);
         eventsFooter = findViewById(R.id.eventsFooter);
         supportFooter = findViewById(R.id.supportFooter);
+        relaot = findViewById(R.id.relaot);
+        image_aot = findViewById(R.id.image_aot);
+        aot = findViewById(R.id.aot);
+        reldate = findViewById(R.id.reldate);
+        image_date = findViewById(R.id.image_date);
+        date = findViewById(R.id.date);
+        relbeach = findViewById(R.id.relbeach);
+        image_beach = findViewById(R.id.image_beach);
+        beach = findViewById(R.id.beach);
+        relburger = findViewById(R.id.relburger);
+        image_burger = findViewById(R.id.image_burger);
+        burger = findViewById(R.id.burger);
+        relcoca = findViewById(R.id.relcoca);
+        image_coca = findViewById(R.id.image_coca);
+        coca = findViewById(R.id.coca);
+        relgradient = findViewById(R.id.relgradient);
+        image_gradient = findViewById(R.id.image_gradient);
+        gradient = findViewById(R.id.gradient);
+        relnoragami = findViewById(R.id.relnoragami);
+        image_noragami = findViewById(R.id.image_noragami);
+        noragami = findViewById(R.id.noragami);
+        relsao = findViewById(R.id.relsao);
+        image_sao = findViewById(R.id.image_sao);
+        sao = findViewById(R.id.sao);
+        linear9 = findViewById(R.id.linear9);
+        linear10 = findViewById(R.id.linear10);
+        linear11 = findViewById(R.id.linear11);
 
     }
 
     public void userinterface() {
-        icon_hamburger.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
 
-        see_all.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
-
-        about.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        aboutFooter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        support.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        supportFooter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        products.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        productFooter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        careers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        careersFooter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        events.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        eventsFooter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
     }
 }
